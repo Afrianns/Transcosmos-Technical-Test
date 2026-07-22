63b1e83ef30031f86804f916627b7399568af7e1
afrianns <hanifnafrian12@gmail.com>
2026-07-22 22:44:04 +0700

    Refactor HTML structure and improve JavaScript functionality
    - Updated HTML to use self-closing tags where applicable for better readability.
    - Enhanced the JavaScript code for the slider and dropdown menus by improving formatting and consistency.
    - Simplified event listeners for dropdown menus and tab functionality.
    - Ensured proper indentation and spacing for better code maintainability.

--- js/main.js
+++ js/main.js
@@ -1,77 +1,76 @@
 // slider
-$(document).ready(function(){
-    $('#slider-m-wrapper').slick({
-        dots: true,
-        prevArrow: $('.slick-prev'),
-        appendDots: $("#dots-wrapper"),
-        nextArrow: $('.slick-next'),
-    });
-})
+$(document).ready(function () {
+  $("#slider-m-wrapper").slick({
+    dots: true,
+    prevArrow: $(".slick-prev"),
+    appendDots: $("#dots-wrapper"),
+    nextArrow: $(".slick-next"),
+  });
+});
 
 // toggle menu
 // header version
 const buttonDropdown = document.getElementById("toggle-dropdown-menu");
-const menuDropdown = document.getElementById("menu-dropdown")
+const menuDropdown = document.getElementById("menu-dropdown");
 
 // footer version
-const buttonDropdownFooter = document.getElementById("toggle-dropdown-menu-footer");
-const menuDropdownFooter = document.getElementById("menu-dropdown-footer")
+const buttonDropdownFooter = document.getElementById(
+  "toggle-dropdown-menu-footer",
+);
+const menuDropdownFooter = document.getElementById("menu-dropdown-footer");
 
 // header
 buttonDropdown.addEventListener("click", () => {
-    menuDropdown.classList.toggle('show-menu-dropdown')
+  menuDropdown.classList.toggle("show-menu-dropdown");
 
-    if(menuDropdown.classList.contains("show-menu-dropdown")){
-        buttonDropdown.innerHTML = "Close"
-    } else{
-        buttonDropdown.innerHTML = "Menu"
-    }
-})
+  if (menuDropdown.classList.contains("show-menu-dropdown")) {
+    buttonDropdown.innerHTML = "Close";
+  } else {
+    buttonDropdown.innerHTML = "Menu";
+  }
+});
 
-// footer 
+// footer
 buttonDropdownFooter.addEventListener("click", () => {
-    menuDropdownFooter.classList.toggle('show-menu-dropdown')
+  menuDropdownFooter.classList.toggle("show-menu-dropdown");
 
-    if(menuDropdownFooter.classList.contains("show-menu-dropdown")){
-        buttonDropdownFooter.innerHTML = "Close"
-    } else{
-        buttonDropdownFooter.innerHTML = "Menu"
-    }
-})
-
+  if (menuDropdownFooter.classList.contains("show-menu-dropdown")) {
+    buttonDropdownFooter.innerHTML = "Close";
+  } else {
+    buttonDropdownFooter.innerHTML = "Menu";
+  }
+});
 
 // tabbing function
-const brandingLink = document.getElementById("branding-link")
-const designLink = document.getElementById("design-link")
-const marketingLink =document.getElementById("marketing-link")
+const brandingLink = document.getElementById("branding-link");
+const designLink = document.getElementById("design-link");
+const marketingLink = document.getElementById("marketing-link");
 
-const brandingContent = document.getElementById("branding-content")
-const designContent = document.getElementById("design-content")
-const marketingContent =document.getElementById("marketing-content")
-
+const brandingContent = document.getElementById("branding-content");
+const designContent = document.getElementById("design-content");
+const marketingContent = document.getElementById("marketing-content");
 
 brandingLink.addEventListener("click", (e) => {
-    handleChangeTab(brandingContent, brandingLink)
-})
+  handleChangeTab(brandingContent, brandingLink);
+});
 
 designLink.addEventListener("click", (e) => {
-    handleChangeTab(designContent, designLink)
-})
+  handleChangeTab(designContent, designLink);
+});
 
 marketingLink.addEventListener("click", (e) => {
-    handleChangeTab(marketingContent, marketingLink)
-})
-
+  handleChangeTab(marketingContent, marketingLink);
+});
 
 function handleChangeTab(content, link) {
-    [brandingLink, designLink, marketingLink].forEach(el => {
-        el.classList.remove("active-link")
-    });
+  [brandingLink, designLink, marketingLink].forEach((el) => {
+    el.classList.remove("active-link");
+  });
 
-    [brandingContent, designContent, marketingContent].forEach(el => {
-        el.classList.remove("show-component")
-    });
+  [brandingContent, designContent, marketingContent].forEach((el) => {
+    el.classList.remove("show-component");
+  });
 
-    content.classList.add("show-component")
-    link.classList.add("active-link")
+  content.classList.add("show-component");
+  link.classList.add("active-link");
 }
